adc67f37721255504bf2c2754946442fbeb8231e
Evan Jones <evan.jones518@gmail.com>
2022-09-21 16:15:29 -0500

    Added all of the necessary math functions and prompts.
    Problems to consider: input needs to have .strip() applied so that they
    dont misbehave. Additionally, it would be useful to reconfigure the
    structure of the program so that it functions more as a calculator which
    runs until commanded otherwise. Also, the prompts for input should be
    moved around so that it makes a bit more sense.

--- calculator.py
+++ calculator.py
@@ -1,12 +1,54 @@
-#!/usr/bin/env python3
+#!C:/Users/evanj/AppData/Local/Programs/Python/Python310/python.exe
 
 import math
+import sys
 
-function = input("Which math function do you want to use? \n >Square \n\n >>>")
-number = input("Which number do you want to use? \n\n>>>")
+def calculation(number1, number2, operator):
+	output = 0
+	
+	if operator.lower() == 'add':
+		output = int(number1) + int(number2)
+
+	if operator.lower() == 'subtract':
+		output = int(number1) - int(number2)
+
+	if operator.lower() == 'multiply':
+		output = int(number1) * int(number2)
+
+	if operator.lower() == 'divide':
+		output = int(number1) / int(number2)
+
+	if operator.lower() == 'power':
+		output = int(number1) ** int(number2)
+
+	return print(output)
+
+
+print("Welcome! This is a simple calculator which operates on two numbers.")
+function = ""
+number1 = ""
+number2 = ""
 output = 0
+operations = ['add', 'subtract', 'multiply', 'divide', 'power']
 
-if function.lower() == "square":
-	output = math.pow(int(number), 2)
+"""Checks if input is valid"""
+while True:
+	function = input("Which math function do you want to use? \n >{} \n\n >>>".format(operations))
+	if function.lower() not in operations:
+		print("Invalid input!")
+		continue
 
-print(output)
+	elif function.lower() in operations:
+		break
+
+while True:
+	number1 = input("Which number do you want to use? \n\n>>>")
+	number2 = input("WHich additional number do you want to use? \n\n>>>")
+	if number1.isnumeric() == False or number2.isnumeric() == False:
+		print("Invalid input!")
+		continue
+	
+	if number1.isnumeric() == True and number2.isnumeric() == True:
+		break
+
+calculation(number1, number2, function)
