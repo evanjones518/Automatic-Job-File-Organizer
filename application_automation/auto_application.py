d9948a2e1b49af159ff8189e8e5cbefdfc2c467b
Evan Jones <evan.jones518@gmail.com>
2022-10-20 15:19:43 -0500

    Application is functional. The only thing which needs work is the CSV generation.

---
+++ application_automation/auto_application.py
@@ -0,0 +1,96 @@
+import os
+import csv
+import docx
+import configparser
+import tkinter as tk
+from tkinter import filedialog
+
+
+def add_application(position, employer, date_applied):
+
+    os.chdir(os.getcwd() + '\job applications')
+    if not os.path.exists(os.getcwd() + '\\' + employer):
+        print("Adding employer folder for: " + employer)
+        os.mkdir(employer)
+    os.chdir(os.getcwd() + '\\' + employer)
+    
+    cv_file_name = 'CV, ' + position + ', ' + employer + '.docx'
+    #Checks if the position document already exists.
+    if not os.path.isfile(os.getcwd() + '\\' + cv_file_name):
+        print('Creating new CV document.')
+        cv = docx.Document()
+        cv.save(cv_file_name)
+    
+    #This returns the CWD to the home directory of the program once the additions are made.
+    os.chdir(file_path)
+
+    with open('jobs_applied.csv', 'w') as csvfile:
+        field_names = ["Jobs", 'Employer', 'Phone Number', 'Applied?', 'Contacted?', 'Interview?', 'Offer?']
+        writer = csv.DictWriter(csvfile, field_names)
+        writer.writerow({'Jobs': position, 'Employer': employer, 'Phone Number': None, 'Applied?': date_applied, 'Contacted?': None, 'Interview?': None, 'Offer?': None})
+    
+#Boolean for program continuation in a while loop.
+##print(os.path.dirname(__file__))
+
+cont_program = True
+root = tk.Tk()
+root.withdraw() # Prevents an empty tkinter window from opening
+config_file = configparser.ConfigParser() #Create configparser object
+
+
+## Create a cconfig file. If there is not a config file, prompt the user to pick a directory to store work files. ##
+print("Pick a directory to store job files:")
+
+#Picks a file path for the program to operate from
+file_path = filedialog.askdirectory()
+
+print('You have selected: ', file_path, '\n')
+os.chdir(file_path)
+
+#Checks if job applications folder is present
+if not os.path.exists(os.getcwd() + "\job applications"):
+    print('Creating "Job Applications" folder.')
+    os.mkdir('job applications')
+
+#Checks if resumes folder is present
+if not os.path.exists(os.getcwd() + '\\resumes'):
+    print('Creating "Resumes" folder.')
+    os.mkdir('resumes')
+#Checks to see if jobs_applied csv is present and creates one if not.
+#***UNDER CONSTRUCTION***
+if not os.path.isfile(os.getcwd() + "\\jobs_applied.csv"):
+    print("Creating new jobs applied csv.")
+    with open('jobs_applied.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Jobs", 'Employer', 'Phone Number', 'Applied?', 'Contacted?', 'Interview?', 'Offer?'])
+
+#Main body of the UX
+while cont_program == True:
+    user_in = input("What would you like to do?\n[A] Add Application\n[B] View Job Applications Folder\n[C] View Resume Folder\n[D] View Application Spreadsheet\n[X] Exit Program\n>>> ")
+    
+
+    if user_in.lower() == 'a':
+        position = input("What is the position called?\n>>> ")
+        employer = input("What is the employer called?\n>>> ")
+        date_applied = input("What is the application date?\n>>> ")
+        add_application(position, employer, date_applied)
+
+    elif user_in.lower() == 'b':
+        print('Opening Job Applications folder...\n')
+        os.startfile(os.getcwd() + '\\job applications')
+
+    elif user_in.lower() == 'c':
+        print('Opening Resumes folder...\n')
+        os.startfile(os.getcwd() + '\\resumes')
+
+    elif user_in.lower() == 'd':
+        print('Opening jobs_applied.csv...\n')
+        os.startfile('jobs_applied.csv')
+
+    elif user_in.lower() == 'x':
+        cont_program = False
+        break
+    
+    #This always prints unless x is typed. I need to replace the above ifs with elifs.
+    else:
+        print('Invalid input!\n')
